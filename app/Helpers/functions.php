4190e75931a2ee7b712a6f90d43d5bd4ea974c7e
yangming <748894407@qq.com>
2018-12-11 13:42:37 +0800

    配置单独路由

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,16 +1,16 @@
 <?php
 
-    /*
-    * 批量添加
-    * $tableName   表名
-    * $data        更新数据
-    * $connect     库名
-    */
-function addAll($table,$data,$connect='')
+/*
+* 批量添加
+* $tableName   表名
+* $data        更新数据
+* $connect     库名
+*/
+function addAll($table, $data, $connect = '')
 {
-    if(!empty($connect)){
-        $rs =DB::connection($connect)->table($table)->insert($data);
-    }else{
+    if (!empty($connect)) {
+        $rs = DB::connection($connect)->table($table)->insert($data);
+    } else {
         $rs = DB::table($table)->insert($data);
     }
     return $rs;
@@ -23,7 +23,7 @@ function addAll($table,$data,$connect='')
  * $multipleData 更新数据
  * 参数，表名，数组
  */
-function updateBatch($tableName = "", $multipleData = [],$connect='')
+function updateBatch($tableName = "", $multipleData = [], $connect = '')
 {
 
     if ($tableName && !empty($multipleData)) {
@@ -31,7 +31,7 @@ function updateBatch($tableName = "", $multipleData = [],$connect='')
         $referenceColumn = $updateColumn[0]; //e.g id
         unset($updateColumn[0]);
         $whereIn = "";
-        $q = "UPDATE " . $tableName . " SET ";
+        $q       = "UPDATE " . $tableName . " SET ";
         foreach ($updateColumn as $uColumn) {
             $q .= $uColumn . " = CASE ";
 
@@ -45,9 +45,9 @@ function updateBatch($tableName = "", $multipleData = [],$connect='')
         }
         $q = rtrim($q, ", ") . " WHERE " . $referenceColumn . " IN (" . rtrim($whereIn, ', ') . ")";
         // Update
-        if(!empty($connect)){
+        if (!empty($connect)) {
             return DB::connection($connect)->update(DB::raw($q));
-        }else{
+        } else {
             return DB::update(DB::raw($q));
         }
 
@@ -56,52 +56,62 @@ function updateBatch($tableName = "", $multipleData = [],$connect='')
     }
 
 }
-function upload(Request $request){
+
+/*
+ * 但文件上传
+ */
+function upload(Request $request)
+{
     if ($request->hasFile('photo') && $request->file('photo')->isValid()) {
-        $photo = $request->file('photo');
-        $extension = $photo->extension();
+        $photo        = $request->file('photo');
+        $extension    = $photo->extension();
         $store_result = $photo->storeAs('photo', 'test.jpg');//设置图片目录
-        $output = [
-            'extension' => $extension,
-            'store_result' => $store_result
+        $output       = [
+            'extension'    => $extension,
+            'store_result' => $store_result,
         ];
-        print_r($output);exit();
+        return $output;
     }
     exit('未获取到上传文件或上传过程出错');
 }
-function multiUploadImg(Request $request){
+
+/*
+ * 多文件上传
+ */
+function multiUploadImg(Request $request)
+{
     // 重组数组，子函数
-    function reArrayFiles( $file_post ) {
+    function reArrayFiles($file_post)
+    {
 
-        $file_ary = array();
+        $file_ary   = [];
         $file_count = count($file_post['name']);
-        $file_keys = array_keys($file_post);
+        $file_keys  = array_keys($file_post);
 
-        for ($i=0; $i<$file_count; $i++) {
+        for ($i = 0; $i < $file_count; $i++) {
             foreach ($file_keys as $key) {
                 $file_ary[$i][$key] = $file_post[$key][$i];
             }
         }
         return $file_ary;
     }
-    $imgFiles = $_FILES['photo']; // 与前端页面中的 input name=“filesToUpload[]” 相对应
-    $uploadedFiles = array(); // 返回值
 
-    if(!empty($imgFiles))
-    {
-        $img_desc = reArrayFiles( $imgFiles );
-        $destinationPath = UPLOAD_URL.'app/photo/';
-        foreach( $img_desc as $img )
-        {
-            $savedFile = $destinationPath.date('YmdHis',time()).mt_rand().'.'.pathinfo( $img['name'], PATHINFO_EXTENSION );
-            move_uploaded_file($img['tmp_name'],  $savedFile);
-            $img['savedFile'] = $savedFile ;
-            array_push( $uploadedFiles, $img );
+    $imgFiles      = $_FILES['photo']; // 与前端页面中的 input name=“filesToUpload[]” 相对应
+    $uploadedFiles = []; // 返回值
+
+    if (!empty($imgFiles)) {
+        $img_desc        = reArrayFiles($imgFiles);
+        $destinationPath = UPLOAD_URL . 'app/photo/';
+        foreach ($img_desc as $img) {
+            $savedFile = $destinationPath . date('YmdHis', time()) . mt_rand() . '.' . pathinfo($img['name'], PATHINFO_EXTENSION);
+            move_uploaded_file($img['tmp_name'], $savedFile);
+            $img['savedFile'] = $savedFile;
+            array_push($uploadedFiles, $img);
         }
     }
 
     $allowed_extensions = ["png", "jpg", "gif"];
     // TODO 判断文件类型
 
-    return ['uploadedFiles' => $uploadedFiles ];
+    return ['uploadedFiles' => $uploadedFiles];
 }
--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -38,10 +38,10 @@ class HomeController extends Controller
         $test=new LaravelTest();
         $users_ucenyer= $test->getAll();
         $array= [
-            ['id'=>1,'name'=>'eee'],
-            ['id'=>2,'name'=>'ddd'],
+            ['name'=>'eee'],
+            ['name'=>'ddd'],
         ];;
-//        $data->addAll('laravel_test',$array);
+       addAll('laravel_test',$array);
 //        $data->addAll('laravel_test',$array,'mysql_ucenter');
 //        $data->updateBatch('laravel_test',$array);
 //        $data->updateBatch('laravel_test',$array,'mysql_ucenter');
@@ -52,55 +52,5 @@ class HomeController extends Controller
     }
     public function show(){
         return view('Home.show');
-    }
-    public function upload(Request $request){
-        if ($request->hasFile('photo') && $request->file('photo')->isValid()) {
-            $photo = $request->file('photo');
-            $extension = $photo->extension();
-            $store_result = $photo->storeAs('photo', 'test.jpg');//设置图片目录
-            $output = [
-                'extension' => $extension,
-                'store_result' => $store_result
-            ];
-            print_r($output);exit();
-        }
-        exit('未获取到上传文件或上传过程出错');
-    }
-    public function multiUploadImg(Request $request){
-        // 重组数组，子函数
-        function reArrayFiles( $file_post ) {
-
-            $file_ary = array();
-            $file_count = count($file_post['name']);
-            $file_keys = array_keys($file_post);
-
-            for ($i=0; $i<$file_count; $i++) {
-                foreach ($file_keys as $key) {
-                    $file_ary[$i][$key] = $file_post[$key][$i];
-                }
-            }
-
-            return $file_ary;
-        }
-        $imgFiles = $_FILES['photo']; // 与前端页面中的 input name=“filesToUpload[]” 相对应
-        $uploadedFiles = array(); // 返回值
-
-        if(!empty($imgFiles))
-        {
-            $img_desc = reArrayFiles( $imgFiles );
-            $destinationPath = UPLOAD_URL.'app/photo/';
-            foreach( $img_desc as $img )
-            {
-                $savedFile = $destinationPath.date('YmdHis',time()).mt_rand().'.'.pathinfo( $img['name'], PATHINFO_EXTENSION );
-                move_uploaded_file($img['tmp_name'],  $savedFile);
-                $img['savedFile'] = $savedFile ;
-                array_push( $uploadedFiles, $img );
-            }
-        }
-
-        $allowed_extensions = ["png", "jpg", "gif"];
-        // TODO 判断文件类型
-
-        return ['uploadedFiles' => $uploadedFiles ];
     }
 }
